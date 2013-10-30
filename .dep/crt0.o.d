149b4c7cf72c83f21ebd8e4919eee0f4678efc4a
ElectricTomato <matthew.ickstadt@gmail.com>
2013-10-30 17:17:35 -0500

    Initial Commit.

---
+++ .dep/crt0.o.d
@@ -0,0 +1,23 @@
+build/obj/crt0.o: \
+ /home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/crt0.c \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h:
---
+++ .dep/testqueues.o.d
@@ -0,0 +1,117 @@
+build/obj/testqueues.o: /home/matt/arm_cortex/ChibiOS/test/testqueues.c \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/ch.h chconf.h \
+ /home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ /home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chlists.h \
+ /home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chcore.h \
+ /home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/STM32F0xx/cmparams.h \
+ /home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chcore_v6m.h \
+ /home/matt/arm_cortex/ChibiOS/os/ports/common/ARMCMx/nvic.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chsys.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chvt.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chschd.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chsem.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chbsem.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chmtx.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chcond.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chevents.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chmsg.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chmboxes.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chmemcore.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chheap.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chmempools.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chthreads.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chdynamic.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chregistry.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chinline.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chqueues.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chstreams.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chfiles.h \
+ /home/matt/arm_cortex/ChibiOS/os/kernel/include/chdebug.h \
+ /home/matt/arm_cortex/ChibiOS/test/test.h
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/ch.h:
+
+chconf.h:
+
+/home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stddef.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h:
+
+/home/matt/arm_cortex/gcc-arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chlists.h:
+
+/home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chcore.h:
+
+/home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/STM32F0xx/cmparams.h:
+
+/home/matt/arm_cortex/ChibiOS/os/ports/GCC/ARMCMx/chcore_v6m.h:
+
+/home/matt/arm_cortex/ChibiOS/os/ports/common/ARMCMx/nvic.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chsys.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chvt.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chschd.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chsem.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chbsem.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chmtx.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chcond.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chevents.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chmsg.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chmboxes.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chmemcore.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chheap.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chmempools.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chthreads.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chdynamic.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chregistry.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chinline.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chqueues.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chstreams.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chfiles.h:
+
+/home/matt/arm_cortex/ChibiOS/os/kernel/include/chdebug.h:
+
+/home/matt/arm_cortex/ChibiOS/test/test.h:
